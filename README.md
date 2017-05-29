03e43e37244adae5864be450839351a39a475978
Furkan Akgün <akgunfu@gmail.com>
2017-05-29 14:49:22 +0300

    Added data_manager.py + Minor Fixes Data manager handles data and normalize it to later use in learning model Updated README file for data_manager.py Updated requirements.txt Removed /data/src path, now instead we have /src and /data seperately

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Kaggle-Competition-Classifier
-A classifier project to classify Kaggle competition data, Term Project of Learning from Data class Spring '17
+A multiclass classifier project to classify Kaggle competition data, Term Project of Learning from Data class Spring '17
+
+1- Data Normalization
+Data we have has 93 features and 9 classes. Each classes has name "Class_1, Class_2" etc. So in the first part,
+in data_manager.py, we are dealing with changing data to a for which we can then use in any learning model. First we change
+class labels to integers and then binarized them.
+
+2- Learning Model
---
+++ src/data_manager.py
@@ -0,0 +1,31 @@
+import pandas as pd     # For Data Manipulation
+
+from keras.utils import np_utils    # For Binarization
+from sklearn.preprocessing import LabelEncoder  # For Encoding Labels to Integers
+
+
+class data_manager():
+
+    def __init__(self):
+        # Read Test and Training Data From the Given Path
+        self.train_df = pd.read_csv('./data/train.csv')
+        self.test_df = pd.read_csv('./data/test.csv')
+
+    # Return to Input X and Output Y
+    def get_data(self):
+        # Encoding Labels to Integer Values
+        y = self.train_df['target'].values
+        encoder = LabelEncoder()
+        encoder.fit(y)
+        encoded_y = encoder.transform(y)
+
+        X = self.train_df.drop(['id', 'target'], axis = 1).values
+        Y = np_utils.to_categorical(encoded_y).astype(int)  # Binarization of Label Values
+
+        return (X,Y)
+
+dm = data_manager()
+x,y = dm.get_data()
+
+print(x)
+print(y)
